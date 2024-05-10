5682b6c65a707d6fa47d96c93be119c225156311
cjsmi <ClarenceSmith90@gmail.com>
2024-05-10 18:51:52 -0400

    global scores as default

--- src/index.js
+++ src/index.js
@@ -64,7 +64,7 @@ document.addEventListener('DOMContentLoaded', () => {
         });
     }
 
-    buildHighScoreList();
+    buildHighScoreList('global');
 
     
     const highScoreSelector = document.getElementById('high-score-selector');
